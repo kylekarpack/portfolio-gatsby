d777ee17fef415870386a714780bae39b1a2cb2e
Kyle Karpack <kylekarpack@gmail.com>
2021-05-23 07:04:22 -0700

    Tailwind start

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,6 +12,7 @@ module.exports = {
 	},
 	plugins: [
 		"gatsby-plugin-react-helmet",
+		"gatsby-plugin-postcss",
 		"gatsby-plugin-styled-components",
 		{
 			resolve: "gatsby-source-google-docs",
--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,30 +1,10 @@
 import React from "react";
 import PropTypes from "prop-types";
-import { ThemeProvider, createGlobalStyle } from "styled-components";
+import { ThemeProvider } from "styled-components";
 import SEO from "./SEO";
 import Navigation from "./Navigation";
 import Footer from "./Footer";
 import theme from "../../config/theme";
-import reset from "../styles/reset";
-
-const GlobalStyle = createGlobalStyle`
-  ${reset}
-  html, input, textarea, button {
-    font-family: "proxima-nova", -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
-	}
-	body {
-		background: rgb(245,247,250);
-	}
-  h1, h2, h3, h4, h5, h6, nav a {
-		font-family: "proxima-nova", -apple-system, BlinkMacSystemFont, "Segoe UI", sans-serif;
-  }
-	h1, h2, h3, h4, h5, h6 {
-		font-weight: 700;
-	}
-  .nav-active {
-    box-shadow: inset 0 -5px 0 ${theme.brand.primary} !important;
-  }
-`;
 
 // We can pass customSEO here to not include the <SEO> component twice. This prop is 'true' on the project template
 // as the SEO component there passes in some additional things. Otherwise things would be inserted two times
@@ -34,7 +14,6 @@ const Layout = ({ children, pathname, customSEO }) => (
 		<>
 			<link rel="stylesheet" href="https://use.typekit.net/kia0axj.css" />
 			{!customSEO && <SEO pathname={pathname} />}
-			<GlobalStyle />
 			<Navigation />
 			{children}
 			<Footer />
