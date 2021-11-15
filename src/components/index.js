252b692252c2681c1ba84854c8a5aeaed9072b60
Kyle Karpack <kylekarpack@gmail.com>
2021-11-14 17:41:52 -0800

    Move contact pages around

--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,10 @@
 import BGImage from "./BGImage";
 import Container from "./Container";
+import Content from "./Content";
 import Footer from "./Footer";
 import Layout from "./Layout";
 import Navigation from "./Navigation";
 import ProjectItem from "./ProjectItem";
 import SEO from "./SEO";
 
-export { BGImage, Container, Footer, Layout, Navigation, ProjectItem, SEO };
+export { BGImage, Container, Content, Footer, Layout, Navigation, ProjectItem, SEO };
--- src/pages/contact/index.jsx
+++ src/pages/contact/index.jsx
@@ -1,35 +1,8 @@
-import React from "react";
-import PropTypes from "prop-types";
-import styled from "styled-components";
-import { Container, Layout } from "../components";
-import ReactMapGL from "react-map-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
-
-const Content = styled(Container)`
-	margin: 0 auto;
-	input,
-	textarea {
-		padding: 0.75em 1em;
-		width: 100%;
-		display: block;
-		margin-bottom: 1em;
-		font-size: 1rem;
-		background: #f2f4f5;
-		border: none;
-		outline: none;
-		box-shadow: 0 0 1px rgba(0, 0, 0, 0.2);
-		&:focus {
-			box-shadow: 0 0 0 2px ${(props) => props.theme.brand.primary};
-		}
-	}
-	button {
-		background: ${(props) => props.theme.brand.primary};
-		color: #fff;
-		padding: 0.5em 2em;
-		font-size: 1em;
-		border: none;
-	}
-`;
+import PropTypes from "prop-types";
+import React from "react";
+import ReactMapGL from "react-map-gl";
+import { Content, Layout } from "../../components";
 
 class ContactPage extends React.Component {
 	_onViewportChange = (viewport) => this.setState({ viewport });
